7722a0945a828562374e29d5b676b5cf87e13379
Gianluca Grancic Lapasini <grancicg@gmail.com>
2021-02-13 19:28:22 -0300

    Agregado el ejer 21

--- ej_21.py
+++ ej_21.py
@@ -3,3 +3,34 @@ Hacer un algoritmo que permita ingresar N números y que luego calcule la suma y
 números ingresados.
 """
 
+cantidad = 0
+suma = 0
+
+continuar = True
+
+while continuar == True:
+
+    opcion = input('Desea ingresar un numero?\nOPCION (S/N): ')
+
+    if opcion.upper() == 'S':
+
+        numero = int(input('Ingresa un número: '))
+
+        suma += numero
+        cantidad += 1
+
+    elif opcion.upper() == 'N':
+
+        break
+
+    else:
+
+        print('Opcion incorrecta!!!!!')
+
+if suma != 0 and cantidad != 0:
+
+    promedio = suma / cantidad
+    print('La suma de los números es:', suma)
+    print('El promedio es:', promedio)
+else:
+    print('Error. No se ha ingresado ningún número.')
